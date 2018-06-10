031be82f2be4729989277f4d65f75fb93cafbcec
Ada Hamilton <gl_mihoubi@esi.dz>
2018-06-10 18:29:30 +0100

    fixed the modal display bug

--- js/notes.js
+++ js/notes.js
@@ -1,26 +1,5 @@
 var modal = document.getElementById("add-note-modal");
 
-document.getElementById("add-note-btn").addEventListener("click", displayModal());
-document.getElementById("cancel").addEventListener("click", cancel());
-document.getElementById("create-note").addEventListener("click", createNote());
-
-function displayModal(){
-   modal.style.display = "block";
-}
-
-function cancel(){
-    // hide modal
-    modal.style.display = "none";
-}
-
-function createNote(){
-    // TODO save user input and create new note object and save it in the local storage
-    // hide modal 
-    modal.style.display = "none";
-}
-/*
-var modal = document.getElementById('create-note-modal');
-
 var addNoteBtn = document.getElementById('add-note-btn');
 
 var createNoteBtn = document.getElementById("create-note");
@@ -40,7 +19,7 @@ createNoteBtn.onclick = function() {
     // hide modal
     modal.style.display = "none";
 }
-*/
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
