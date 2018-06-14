3fc364b806c248deb800b8f267b2db70713d7f0c
Ada Hamilton <gl_mihoubi@esi.dz>
2018-06-14 23:56:33 +0100

    working on the display note feature

--- js/notes.js
+++ js/notes.js
@@ -1,10 +1,10 @@
-var modal = document.getElementById("add-note-modal");
+let modal = document.getElementById("add-note-modal");
 
-var addNoteBtn = document.getElementById('add-note-btn');
+let addNoteBtn = document.getElementById('add-note-btn');
 
-var createNoteBtn = document.getElementById("create-note");
+let createNoteBtn = document.getElementById("create-note");
 
-var cancelBtn = document.getElementById("cancel");
+let cancelBtn = document.getElementById("cancel");
 
 addNoteBtn.onclick = function() {
     modal.style.display = "block";
@@ -15,14 +15,13 @@ cancelBtn.onclick = function() {
 }
 
 createNoteBtn.onclick = function() {
-    // TODO save user input and create new note object and save it in the local storage
-    var noteTitle = document.getElementById("title").value;
-    var noteScore = document.getElementById("noteScore").value;
-    var noteText = document.getElementById("noteBody").value;
-    var noteGoal = document.getElementById("select-goal").value;
+    let noteTitle = document.getElementById("title").value;
+    let noteScore = document.getElementById("noteScore").value;
+    let noteText = document.getElementById("noteBody").value;
+    let noteGoal = document.getElementById("select-goal").value;
     // for the moment multiple labels aren't handled
-    var noteLabel = document.getElementById("add-labels").value;
-    var note = {
+    let noteLabel = document.getElementById("add-labels").value;
+    let note = {
         title: noteTitle,
         score: noteScore,
         text: noteText,
@@ -30,7 +29,55 @@ createNoteBtn.onclick = function() {
         label: noteLabel
     }
     saveNote(note);
-    // TODO display the notein the notes page
+    // TODO display the note in the notes page
+    // creating the note
+    let t = document.createElement("h3");
+    let titleText = document.createTextNode(noteTitle);
+    t.id = "note-title";
+    t.appendChild(titleText);
+    let s = document.createElement("h3");
+    s.id = "note-score";
+    let scorevalue = document.createTextNode(noteScore);
+    s.appendChild(s);
+    let header = document.createElement("header");
+    header.id = "note-header";
+    header.appendChild(t);
+    header.appendChild(s);
+    let noteBody = document.createElement("p");
+    noteBody.id = "note-body";
+    let p = document.createTextNode(noteText);
+    noteBody.appendChild(p);
+    let g = document.createElement("p");
+    let goalName = document.createTextNode(noteGoal);
+    g.appendChild(goalName);
+    g.id = "note-goal";
+    let l = document.createElement("p");
+    let labelName = document.createTextNode(noteLabel);
+    l.appendChild(labelName);
+    l.id = "note-label";
+    let editBtn = document.createElement("button");
+    let edit = document.createTextNode("edit note");
+    editBtn.appendChild(edit);
+    editBtn.id = "edit-btn";
+    editBtn.type = "button";
+    let deleteBtn = document.createElement("button");
+    let dlt = document.createTextNode("delete note");
+    deleteBtn.appendChild(dlt);
+    deleteBtn.id = "delete-btn";
+    deleteBtn.type = "button";
+    let footer = document.createElement("footer");
+    footer.id = "note-footer";
+    footer.appendChild(g);
+    footer.appendChild(l);
+    footer.appendChild(editBtn);
+    footer.appendChild(deleteBtn);
+    let newNote = document.createElement("div");
+    newNote.id = "new-note";
+    newNote.appendChild(header);
+    newNote.appendChild(noteBody);
+    newNote.appendChild(footer);
+    let notesPage = document.getElementById("notes-page");
+    notesPage.appendChild(newNote);
     // TODO update the labels list in the notes.html 
     // TODO update the goals page: 1/update the goals line chart 2/if a goal is created on the fly from the notes page while creating a new note add the new goal to the goals page and the goals array in the localStorage
     // hide modal
@@ -46,7 +93,7 @@ window.onclick = function(event) {
 
 // saving the note
 function saveNote(note){
-    varnotes = [];
+    letnotes = [];
     if(localStorage.getItem("notes") !== null){
         notes = JSON.parse(localStorage.getItem('notes')); 
     } 
