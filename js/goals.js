3eb09705da95d6b9d6677cf85e42b19f0cafe5b2
CJ Day <daycharles@gmail.com>
2018-06-18 08:56:06 -0400

    Added goals modal

---
+++ js/goals.js
@@ -0,0 +1,6 @@
+// This is the JavaScript for the goals page
+
+let modal = document.getElementById("add-goal-modal");
+let addGoalBtn = document.getElementById("add-goal-btn");
+let createGoalBtn = document.getElementById("create-note");
+let cancelBtn = document.getElementById("cancel");
